064b48d117f04feb496d9bfffaa56bb8bc901091
Charles Lucas Pedigo <charles_pedigo@mymail.eku.edu>
2026-02-10 11:40:43 -0500

    Initial Commit

---
+++ M4Lotto_Ex2_CharlesPedigo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M4Lotto_Ex2_CharlesPedigo
+//
+//  Created by Pedigo, Charles L. on 2/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
